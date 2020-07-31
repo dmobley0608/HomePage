b13954ce4cd1e97e4f30b425464f047c09cdbd48
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2020-07-31 06:38:05 -0400

    Fixed searchbar width

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -5,8 +5,8 @@ import React from 'react';
 function Searchbar(){
 	return(
 		<div className = "tc pt4">
-			<form  method="get" action="http://www.google.com/search">					
-				<input  className = "w-30 tc br-pill " type="text"  name="q" placeholder="What do you need to know?"/>	
+			<form  method="get" action="https://www.google.com/search">					
+				<input style ={{minWidth: "275px"}} className = "w-30 tc br-pill " type="text"  name="q" placeholder="What do you need to know?"/>	
 				<input className=" ml2 br-pill pl2 pr2"  type="submit" value="SEARCH" />				
 				
 				<input type="hidden" name="sitesearch" value="" />
