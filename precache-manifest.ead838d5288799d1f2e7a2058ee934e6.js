20e61333645798ef7f0927928237b278319b01ba
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2020-08-29 09:54:47 -0400

    Updates

---
+++ precache-manifest.ead838d5288799d1f2e7a2058ee934e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f571f3dd98dee33b31673fd3ff1767",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "b5707a83d18a45b490e4",
+    "url": "/homepage/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a3296989b6068f409166",
+    "url": "/homepage/static/css/main.4b921382.chunk.css"
+  },
+  {
+    "revision": "b5707a83d18a45b490e4",
+    "url": "/homepage/static/js/2.10d28387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homepage/static/js/2.10d28387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3296989b6068f409166",
+    "url": "/homepage/static/js/main.e152da8c.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.dc2a45734337e0f6717bd2d478a2e466.js"
+  "/homepage/precache-manifest.ead838d5288799d1f2e7a2058ee934e6.js"
 );
 
 self.addEventListener('message', (event) => {
